84a3716f09c254ab8c51258fda1326e39192fc49
Asaad Hussain <asaadhussain2014@gmail.com>
2024-11-23 22:25:57 +0500

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 import './App.css';
 import Home from './screens/Home.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './screens/Login.jsx';
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import "./index.css"
 
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Footer from '../components/Footer.jsx'
+import Card from '../components/Card.jsx'
+import Carousel from '../components/Carousel.jsx'
 
 export default function Home() {
     return (
         <div>
             <div>
                 <div><Navbar /></div>
-                <div>Body</div>
-                <div><Footer/></div>
+                <div><Carousel/></div>
+                <div className='m-3'><Card /></div>
+                <div><Footer /></div>
             </div>
         </div>
     )
